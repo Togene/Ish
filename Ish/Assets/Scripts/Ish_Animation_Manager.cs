bc3b9481ece1a9899bd3c6458a288463fc7a326a
Togene <jhonnywhiplash@hotmail.com.au>
2017-02-11 19:08:34 +1000

    Added : Main Ish Game

---
+++ Ish/Assets/Scripts/Ish_Animation_Manager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ish_Animation_Manager : MonoBehaviour {
+
+	public Animator anim; //<--- this dude total Hash128 an animator
+	public Kyra_Rules kyraRules;
+	public int numberofPeeps;
+	public bool isWalking, isRight, isLeft, isUp, isDown;
+
+
+
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+
+		//Telling the Animator some shit
+		anim.SetBool("Walking", false);
+
+
+
+		isWalking = false;
+		kyraRules = GetComponent<Kyra_Rules>();
+
+		//kyraRules.isKyraGay = true;
+	}
+
+	void Update () 
+	{
+
+		if(Input.GetKey("w"))
+		{
+			anim.SetBool("Up", true);
+		}
+
+		if(Input.GetKey("a"))
+		{
+			anim.SetBool("Left", true);
+		}
+
+		if(Input.GetKey("d"))
+		{
+			anim.SetBool("Right", true);
+		}
+
+		if(Input.GetKey("s"))
+		{
+			anim.SetBool("Down", true);
+		}
+
+		numberofPeeps ++;
+	}
+}
---
+++ Ish/Assets/Scripts/Kyra_Rules.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kyra_Rules : MonoBehaviour {
+
+	public bool isKyraGay;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		isKyraGay = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
